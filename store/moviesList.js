b23f26e10db63177d6c6ee69ed7be0e37c530ac9
Mestpal <manuestpal@gmail.com>
2020-02-19 19:25:06 +0100

    WIP-store for the movies found

---
+++ store/moviesList.js
@@ -0,0 +1,21 @@
+export const state = {
+  moviesList: []
+}
+
+export const getters = {
+  moviesList: (state) => { return state.moviesList }
+}
+
+export const mutations = {
+  UPDATE_MOVIE_LIST (state, newMovies) {
+    state.moviesList = newMovies
+  }
+}
+
+export const actions = {
+  async getMoviesList ({ commit }, data) {
+    const response = await this.$axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=059b24ced08b9601d550dc1bda480265&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=1&with_genres=${data.genreId}`)
+    console.log(response.data)
+    commit('UPDATE_MOVIE_LIST', response.data.results)
+  }
+}
