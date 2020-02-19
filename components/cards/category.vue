7bf70d4abbf8e76b7c2effe2379cfb5585de8ed1
Mestpal <manuestpal@gmail.com>
2020-02-19 15:47:00 +0100

    First version of the category component

---
+++ components/cards/category.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div
+      style="
+        color:green;
+        background-color:aquamarine;
+        width: 28.3%;
+        min-width:200px;
+        height: auto;
+        min-height:100px;
+        margin: 10px 2.5%;
+        display: inline-block;
+        float: left
+      "
+    >
+      <h2 v-text='data.name'/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+      required: true
+    }
+  }
+}
+</script>
