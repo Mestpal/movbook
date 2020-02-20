56db48f97b39328fc38b5de42b19197a73b654e8
Mestpal <manuestpal@gmail.com>
2020-02-20 12:31:45 +0100

    Reset actions in movieList store

--- store/moviesList.js
+++ store/moviesList.js
@@ -9,13 +9,18 @@ export const getters = {
 export const mutations = {
   UPDATE_MOVIE_LIST (state, newMovies) {
     state.moviesList = newMovies
+  },
+  RESET (state) {
+    state.moviesList = []
   }
 }
 
 export const actions = {
   async getMoviesList ({ commit }, data) {
     const response = await this.$axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=059b24ced08b9601d550dc1bda480265&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=1&with_genres=${data.genreId}`)
-    console.log(response.data)
     commit('UPDATE_MOVIE_LIST', response.data.results)
+  },
+  resetMovieListData ({ commit }) {
+    commit('RESET')
   }
 }
