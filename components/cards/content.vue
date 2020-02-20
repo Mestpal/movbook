938cdc18bfc31d4c4aa571939ab2249565f57988
Mestpal <manuestpal@gmail.com>
2020-02-20 12:35:28 +0100

    Component to show each content of a category

---
+++ components/cards/content.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-flex
+    xs12
+    sm6
+    md4
+    lg4
+    xl3
+  >
+    <v-card
+      :class="'movieListElement'"
+      @click="$emit('prepareContent', data.id)"
+    >
+      <v-card-title
+        v-text="data.title"
+      />
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  }
+}
+</script>
