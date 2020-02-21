9b38a076594ae295072fdd08228c123ddf0d4c45
Mestpal <manuestpal@gmail.com>
2020-02-21 13:38:49 +0100

    page is part of the request and the results are concat with the previous ones

--- store/moviesList.js
+++ store/moviesList.js
@@ -8,7 +8,7 @@ export const getters = {
 
 export const mutations = {
   UPDATE_MOVIE_LIST (state, newMovies) {
-    state.moviesList = newMovies
+    state.moviesList = state.moviesList.concat(newMovies)
   },
   RESET (state) {
     state.moviesList = []
@@ -17,7 +17,7 @@ export const mutations = {
 
 export const actions = {
   async getMoviesList ({ commit }, data) {
-    const response = await this.$axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=059b24ced08b9601d550dc1bda480265&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=1&with_genres=${data.genreId}`)
+    const response = await this.$axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=059b24ced08b9601d550dc1bda480265&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=${data.page}&with_genres=${data.genreId}`)
     commit('UPDATE_MOVIE_LIST', response.data.results)
   },
   resetMovieListData ({ commit }) {
