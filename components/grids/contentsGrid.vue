057479746bef6d677f540af1e1619b6634a1fbb5
Mestpal <manuestpal@gmail.com>
2020-02-22 21:22:30 +0100

    contentsGrid component

---
+++ components/grids/contentsGrid.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout
+    row
+    justify-space-between
+  >
+    <ContentsListElement
+      v-for="(movie, index) in contentsList"
+      :key="index"
+      :data="movie"
+      :imgBase="configuration.images.base_url + configuration.images.poster_sizes[4]"
+      @prepareContent="openContent($event)"
+    />
+  </v-layout>
+</template>
+
+<script>
+import ContentsListElement from '@/components/cards/content'
+
+export default {
+  components: {
+    ContentsListElement
+  },
+  props: {
+    contentsList: {
+      type: Array,
+      default: () => []
+    },
+    configuration: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    openContent (id) {
+      this.$router.push(`/movie/${id}`)
+    }
+  }
+}
+</script>
