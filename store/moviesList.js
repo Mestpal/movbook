28f257ea29fafb6cade22f13e3a024a9b51f7a7e
Mestpal <manuestpal@gmail.com>
2020-02-22 21:26:43 +0100

    request to search a movie  with the external API

--- store/moviesList.js
+++ store/moviesList.js
@@ -20,6 +20,10 @@ export const actions = {
     const response = await this.$axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=059b24ced08b9601d550dc1bda480265&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=${data.page}&with_genres=${data.genreId}`)
     commit('UPDATE_MOVIE_LIST', response.data.results)
   },
+  async getMoviesListLive ({ commit }, data) {
+    const response = await this.$axios.get(`https://api.themoviedb.org/3/search/movie?api_key=059b24ced08b9601d550dc1bda480265&language=en-US&query=${data.query}&page=1&include_adult=false`)
+    commit('UPDATE_MOVIE_LIST', response.data.results)
+  },
   resetMovieListData ({ commit }) {
     commit('RESET')
   }
