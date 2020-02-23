ffa4087237e72ea505972aa06546480f170309bc
Mestpal <manuestpal@gmail.com>
2020-02-23 13:02:52 +0100

    section/_category/_id index page test

---
+++ test/pages/section/_category/_id/index.test.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+
+import index from '@/pages/section/_category/_id/index'
+Vue.use(Vuetify)
+Vue.use(Vuex)
+const localVue = createLocalVue()
+
+describe('section category id index.vue page', () => {
+  const $route = {
+    params: {
+      id: 11111111,
+      category: 'Test'
+    }
+  }
+
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallowMount(index, {
+      localVue,
+      mocks: {
+        $route
+      },
+      methods: {
+        updateSelectedGenre () {
+          return ''
+        }
+      }
+    })
+  })
+
+  it('reders the page', () => {
+    // wrapper.vm.updateSelectedGenre = jest.fn()
+    // wrapper.vm.categoryTitle = jest.fn()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
